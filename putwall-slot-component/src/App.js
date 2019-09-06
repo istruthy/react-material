a544e008c65ff98c33a423b81db92bd8e007ea3e
dlunsford <dlunsford@zulily.com>
2019-09-06 13:04:47 -0700

    first commit

---
+++ putwall-slot-component/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SlotTag from './SlotTag';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SlotTag />
+      </div>
+    );
+  }
+}
+export default App;
